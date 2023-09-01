77b7d0b1390671ee06267b1044bc3eb3487969ce
Krystal <lopezkrystal10@gmail.com>
2023-09-01 12:45:37 -0700

    edit workflow to deploy ses directly and updated body message and added query params

--- handler.js
+++ handler.js
@@ -1,34 +1,50 @@
-'use strict';
+"use strict";
 // top of file
-const AWS = require('aws-sdk');
-const ses = new AWS.SES({ region: 'us-west-1' });
+const AWS = require("aws-sdk");
+const ses = new AWS.SES({ region: "us-west-1" });
 
 module.exports.sendEmail = async (event) => {
-// inside the `exports.handler.sendEmail` function, before the `return` statement
-const params = {
-  Destination: {
-    ToAddresses: ['lopezkrystal10@gmail.com'], // This should be your email address
-  },
-  Message: {
-    Body: {
-      Text: {
-        Data: 'This is a message generated automatically from a Lambda function.',
+  // inside the `exports.handler.sendEmail` function, before the `return` statement
+  const queryParams = event.queryStringParameters || {};
+
+  // select from the query parameters
+  let { email, message, subject } = queryParams;
+
+  if (!email) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "Email is required",
+      }),
+    };
+  }
+  message = message || "This is a message generated automatically from a Lambda function.";
+  subject = subject || "Hello from Lambda";
+
+  const params = {
+    Destination: {
+      ToAddresses: ["lopezkrystal10@gmail.com"], // This should be your email address
+    },
+    Message: {
+      Body: {
+        Text: {
+          Data: "This is a message generated automatically from a Lambda function.",
+        },
+      },
+      Subject: {
+        Data: "Hello from Lambda",
       },
     },
-    Subject: {
-      Data: 'Hello from Lambda',
-    },
-  },
-  Source: 'lopezkrystal10@gmail.com', // This is the email listed in sender. Set it to your email for this practice
-};
-await ses.sendEmail(params).promise();
-// in the object that is `return`ed, replace the `body.message` property with `Email sent to ${params.Destination.ToAddresses}`
+    Source: "lopezkrystal10@gmail.com", // This is the email listed in sender. Set it to your email for this practice
+  };
+  await ses.sendEmail(params).promise();
+  // in the object that is `return`ed, replace the `body.message` property with `Email sent to ${params.Destination.ToAddresses}`
 
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: `Email sent to ${params.Destination.ToAddresses}`,
+        message: `Email sent to ${email} with subject ${subject} and message ${message}`,
         input: event,
       },
       null,
